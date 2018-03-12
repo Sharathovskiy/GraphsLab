be69e8ac29af0c0b8897db7b3b28c8ba80963c46
Sharathovskiy <sharathovskiy@gmail.com>
2018-03-12 14:52:47 +0100

    Updates Simple Form View, adds labels to edges and nodes.

--- GraphService.py
+++ GraphService.py
@@ -6,11 +6,13 @@ class GraphService:
     def __init__(self, data=None):
         self.graph = nx.Graph(data)
 
-    def add_node(self, n, weight=None):
+    def add_node(self, n, label=None, weight=None):
         self.graph.add_node(n, weight=weight)
+        self.graph.node[n]['label'] = label
 
-    def add_edge(self, u, v, weight=None):
+    def add_edge(self, u, v, label=None, weight=None):
         self.graph.add_edge(u, v, weight=weight)
+        self.graph[u][v]['label'] = label
 
     def draw_graph(self):
         nx.draw(self.graph, with_labels=True)
@@ -24,3 +26,9 @@ class GraphService:
 
     def remove_graph(self):
         self.graph = nx.Graph()
+
+    def get_edge_label(self, u, v):
+        return self.graph[u][v]['label']
+
+    def get_node_label(self, n):
+        return self.graph.node[n]['label']
