0e628b4eddc0f6e1ec61afd09aea2422c5a54555
Teteran <kacper3d3@gmail.com>
2018-03-13 19:33:31 +0100

    Added new form for drawing graph based on incidence matrix

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 from tkinter import *
 
-from GraphService import GraphService
+
 from tkinter.ttk import Notebook
 
 from views.SimpleFormAdd import AddGraphSimpleFormViewCreator
 from views.AdjacencyMatrixGraph import AdjacencyMatrixGraph
+from views.IncidenceMatrixGraph import IncidenceMatrixGraph
 
 if __name__ == '__main__':
     root = Tk()
@@ -12,12 +13,15 @@ if __name__ == '__main__':
 
     frame1 = Frame(notebook)
     frame2 = Frame(notebook)
+    frame3 = Frame(notebook)
 
     notebook.add(frame1, text='Frame One')
-    notebook.add(frame2, text='Frame Two')
+    notebook.add(frame2, text='Adjacency Matrix Graph')
+    notebook.add(frame3, text='Incidence Matrix Graph')
     notebook.pack()
 
     AddGraphSimpleFormViewCreator(frame1)
     AdjacencyMatrixGraph(frame2)
+    IncidenceMatrixGraph(frame3)
 
     root.mainloop()
