ab12d6a8351bd61e2080571ad5a50a98371369b6
Teteran <kacper3d3@gmail.com>
2018-03-13 22:08:22 +0100

    Added new form for drawing graph based on adjacency list

--- Main.py
+++ Main.py
@@ -1,10 +1,9 @@
 from tkinter import *
-
-from GraphService import GraphService
 from tkinter.ttk import Notebook
 
 from views.SimpleFormAdd import AddGraphSimpleFormViewCreator
 from views.AdjacencyMatrixGraph import AdjacencyMatrixGraph
+from views.AdjacencyListGraph import AdjacencyListGraph
 
 if __name__ == '__main__':
     root = Tk()
@@ -12,12 +11,15 @@ if __name__ == '__main__':
 
     frame1 = Frame(notebook)
     frame2 = Frame(notebook)
+    frame3 = Frame(notebook)
 
     notebook.add(frame1, text='Frame One')
     notebook.add(frame2, text='Frame Two')
+    notebook.add(frame3, text='Adjacency List')
     notebook.pack()
 
     AddGraphSimpleFormViewCreator(frame1)
     AdjacencyMatrixGraph(frame2)
+    AdjacencyListGraph(frame3)
 
     root.mainloop()
