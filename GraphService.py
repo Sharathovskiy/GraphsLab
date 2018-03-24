1ceabf554c0b1bd20aa9f0b938f9140f2f90f62c
Sharathovskiy <sharathovskiy@gmail.com>
2018-03-24 17:51:05 +0100

    Corrects displaying weights on graph, implements feature that chunks edge list for smaller ones, so they can be displayed in different way

--- GraphService.py
+++ GraphService.py
@@ -12,16 +12,49 @@ class GraphService:
     def add_edge(self, u, v, weight=None):
         self.graph.add_edge(u, v, weight=weight)
 
-
     def draw_graph(self):
-        nx.draw_networkx_edge_labels(self.graph, pos=nx.spring_layout(self.graph), edge_labels=self.get_weights_as_edge_labels())
-        nx.draw(self.graph, with_labels=True)
+        split_edges = self.split_edges_by_weights(3, 5)
+
+        pos = nx.spring_layout(self.graph)
+
+        nx.draw_networkx_nodes(self.graph, pos=pos, node_size=700)
+
+        line_width = 2
+        for key, value in split_edges.items():
+            nx.draw_networkx_edges(self.graph, pos, edgelist=value, width=line_width)
+            line_width += 2
+
+        nx.draw_networkx_edge_labels(self.graph, pos=pos, edge_labels=self.get_edge_weights_dict())
+        nx.draw_networkx_labels(self.graph, pos)
+
+        plt.axis('off')
         plt.show()
 
-    def get_weights_as_edge_labels(self):
+    def split_edges_by_weights(self, chunks, weight_interval):
+        split_edges = {}
+        for i in range(chunks):
+            split_edges[i] = []
+
+        for i in range(chunks):
+            for j, (u, v, d) in enumerate(self.get_edges(True)):
+                w = d['weight']
+
+                if w > chunks * weight_interval:
+                    split_edges[chunks-1].append((u, v))
+                    continue
+
+                if w < weight_interval:
+                    split_edges[0].append((u, v))
+                    continue
+
+                if w > i * weight_interval and w <= (i + 1) * weight_interval:
+                    split_edges[i].append((u, v))
+        return split_edges
+
+    def get_edge_weights_dict(self):
         edge_labels = {}
-        for (u,v, d) in self.get_edges(True):
-            edge_labels[(u,v)] = d['weight']
+        for (u, v, d) in self.get_edges(True):
+            edge_labels[(u, v)] = d['weight']
 
         return edge_labels
 
