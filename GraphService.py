b1cc13530afee13177d5e33e213bceec71720ce2
Sharathovskiy <sharathovskiy@gmail.com>
2018-03-24 15:56:53 +0100

    Removes edge labels and node weights, formats edge labels showing only edges weight value

--- GraphService.py
+++ GraphService.py
@@ -6,29 +6,36 @@ class GraphService:
     def __init__(self, data=None):
         self.graph = nx.Graph(data)
 
-    def add_node(self, n, label=None, weight=None):
-        self.graph.add_node(n, weight=weight)
-        self.graph.node[n]['label'] = label
+    def add_node(self, n, label=None):
+        self.graph.add_node(n, label=label)
 
-    def add_edge(self, u, v, label=None, weight=None):
+    def add_edge(self, u, v, weight=None):
         self.graph.add_edge(u, v, weight=weight)
-        self.graph[u][v]['label'] = label
+
 
     def draw_graph(self):
+        nx.draw_networkx_edge_labels(self.graph, pos=nx.spring_layout(self.graph), edge_labels=self.get_weights_as_edge_labels())
         nx.draw(self.graph, with_labels=True)
         plt.show()
+
+    def get_weights_as_edge_labels(self):
+        edge_labels = {}
+        for (u,v, d) in self.get_edges(True):
+            edge_labels[(u,v)] = d['weight']
+
+        return edge_labels
 
     def get_nodes(self):
         return self.graph.nodes
 
-    def get_edges(self):
-        return self.graph.edges
+    def get_edges(self, data=False):
+        return self.graph.edges(data=data)
 
     def remove_graph(self):
         self.graph = nx.Graph()
 
-    def get_edge_label(self, u, v):
-        return self.graph[u][v]['label']
-
     def get_node_label(self, n):
         return self.graph.node[n]['label']
+
+    def get_edge_weight(self, u, v):
+        return self.graph[u][v]['weight']
--- views/SimpleFormAdd.py
+++ views/SimpleFormAdd.py
@@ -1,73 +1,61 @@
 from tkinter import *
 from GraphService import GraphService
 
+
 # TODO: Handle Enter hit
 class AddGraphSimpleFormViewCreator:
     def __init__(self, master):
         self.graphService = GraphService()
 
-        # GUI ELEMENTS
-
         # NODE COLUMN
-        self.add_node_label = Label(master, text="Add node")
-        self.node_weight_label = Label(master, text="Node weight")
+        self.add_node_label = Label(master, text="Node")
         self.current_nodes_label = Label(master, text="Current nodes: ")
         self.current_nodes_info = Label(master)
         self.node_label = Label(master, text="Node label")
         self.add_node_button = Button(master, text="+", command=self.add_node)
 
         self.add_node_entry = Entry(master)
-        self.node_weight = Entry(master)
         self.node_label_entry = Entry(master)
 
-
         # EDGE COLUMN
-        self.add_edge_label = Label(master, text="Add edge")
+        self.add_edge_label = Label(master, text="Edge")
         self.edge_weight_label = Label(master, text="Edge weight")
         self.current_edges_label = Label(master, text="Current edges: ")
         self.current_edges_info = Label(master)
-        self.edge_label = Label(master, text="Edge label")
         self.add_edge_button = Button(master, text="+", command=self.add_edge)
 
         self.add_edge_entry = Entry(master)
         self.edge_weight = Entry(master)
-        self.edge_label_entry = Entry(master)
 
         # DRAW / REMOVE BUTTONS
         self.draw_graph_button = Button(master, text="Draw graph", command=self.draw_graph)
         self.remove_graph_button = Button(master, text="Remove graph", command=self.remove_graph)
 
-        # GRID
-
-        # NODE COLUMN
+        # NODE COLUMN GRID
 
         # LABELS
         self.add_node_label.grid(row=0, sticky=E)
         self.node_label.grid(row=1, sticky=E)
-        self.node_weight_label.grid(row=2, sticky=E)
-        self.add_node_button.grid(row=3, columnspan=3)
-        self.current_nodes_label.grid(row=4)
-        self.current_nodes_info.grid(row=4, column=1, sticky=W)
+        self.add_node_button.grid(row=2, columnspan=3)
+        self.current_nodes_label.grid(row=3)
+        self.current_nodes_info.grid(row=3, column=1, sticky=W)
 
         # ENTRIES
         self.add_node_entry.grid(row=0, column=1)
         self.node_label_entry.grid(row=1, column=1)
-        self.node_weight.grid(row=2, column=1)
 
-        # EDGE COLUMN
+        # EDGE COLUMN GRID
 
         # LABELS
         self.add_edge_label.grid(row=0, column=2, sticky=E)
-        self.edge_label.grid(row=1, column=2, sticky=E)
-        self.edge_weight_label.grid(row=2, column=2, sticky=E)
-        self.add_edge_button.grid(row=3, column=3, columnspan=3)
-        self.current_edges_label.grid(row=4, column=2)
-        self.current_edges_info.grid(row=4, column=3, sticky=W)
+        self.edge_weight_label.grid(row=1, column=2, sticky=E)
+        self.add_edge_button.grid(row=2, column=3, columnspan=3)
+        self.current_edges_label.grid(row=3, column=2)
+        self.current_edges_info.grid(row=3, column=3, sticky=W)
 
         # ENTRIES
         self.add_edge_entry.grid(row=0, column=3)
-        self.edge_label_entry.grid(row=1, column=3)
-        self.edge_weight.grid(row=2, column=3)
+        self.edge_weight.grid(row=1, column=3)
 
         # DRAW / REMOVE BUTTONS
 
@@ -84,14 +72,11 @@ class AddGraphSimpleFormViewCreator:
     def add_node(self):
         node = self.add_node_entry.get()
         label = self.node_label_entry.get()
-        # TODO: handle non numeric weight strings
-        weight = float(self.node_weight.get())
-        self.graphService.add_node(node, label=label, weight=weight)
-        print(self.graphService.graph.node[node]['label'])
+
+        self.graphService.add_node(node, label=label)
 
         self.erase_entry(self.add_node_entry)
         self.erase_entry(self.node_label_entry)
-        self.erase_entry(self.node_weight)
         self.update_info()
 
     def add_edge(self):
@@ -99,14 +84,11 @@ class AddGraphSimpleFormViewCreator:
         # TODO: handle indexes
         u = data[0]
         v = data[1]
-        label = self.edge_label_entry.get()
         # TODO: handle non numeric weight strings
         weight = float(self.edge_weight.get())
-        self.graphService.add_edge(u, v, label=label, weight=weight)
-        print(self.graphService.graph[u][v]['label'])
+        self.graphService.add_edge(u, v, weight=weight)
 
         self.erase_entry(self.add_edge_entry)
-        self.erase_entry(self.edge_label_entry)
         self.erase_entry(self.edge_weight)
         self.update_info()
 
