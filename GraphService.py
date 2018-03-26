57970a6fccc9dc82437b76fa91cd953859e44ff8
Teteran <kacper3d3@gmail.com>
2018-03-26 09:13:42 +0200

    Added list of ondes and edges + edit filed for Labels and Wights

--- GraphService.py
+++ GraphService.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from tkinter import *
 
 
 class GraphService:
@@ -12,7 +13,14 @@ class GraphService:
     def add_edge(self, u, v, weight=None):
         self.graph.add_edge(u, v, weight=weight)
 
-    def draw_graph(self):
+    def draw_graph(self, nodes_listbox=None, edges_listbox=None):
+        if nodes_listbox and edges_listbox:
+            nodes_listbox.delete(0, END)
+            edges_listbox.delete(0, END)
+            for node in self.get_nodes():
+                nodes_listbox.insert(END, node)
+            for edge in self.get_edges():
+                edges_listbox.insert(END, edge)
         nx.draw(self.graph, with_labels=True)
         plt.show()
 
@@ -20,15 +28,15 @@ class GraphService:
         self.graph = nx.from_numpy_matrix(adjacency_matrix)
         self.draw_graph()
 
-    def draw_graph_from_adjacency_list(self, adjacency_list):
+    def draw_graph_from_adjacency_list(self, adjacency_list, nodes_listbox, edges_listbox):
         self.graph = nx.parse_adjlist(adjacency_list, nodetype=int)
-        self.draw_graph()
+        self.draw_graph(nodes_listbox, edges_listbox)
 
     def get_nodes(self):
-        return self.graph.nodes
+        return self.graph.nodes(data='weight', default='')
 
     def get_edges(self):
-        return self.graph.edges
+        return self.graph.edges(data='label', default='')
 
     def remove_graph(self):
         self.graph = nx.Graph()
